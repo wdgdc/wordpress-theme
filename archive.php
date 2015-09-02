77f4cfdc026b43dfd8d0b466d769633d7557d5cb
Mike Garrett <hello@kernme.org>
2015-09-02 11:44:15 -0400

    Remove Bones References
    Text domain changed to wdg-theme.
    Replaced bones functions with the equivalent in WordPress core.

--- archive.php
+++ archive.php
@@ -4,34 +4,34 @@
 
 	<?php if (is_category()) : ?>
 		<h1 class="archive-title archive-category-title intro-title">
-			<span><?php _e('Posts Categorized:', 'bonestheme'); ?></span> <?php single_cat_title(); ?>
+			<span><?php _e('Posts Categorized:', 'wdg-theme'); ?></span> <?php single_cat_title(); ?>
 		</h1>
 
 	<?php elseif (is_tag()) : ?>
 		<h1 class="archive-title archive-tag-title intro-title">
-			<span><?php _e('Posts Tagged:', 'bonestheme'); ?></span> <?php single_tag_title(); ?>
+			<span><?php _e('Posts Tagged:', 'wdg-theme'); ?></span> <?php single_tag_title(); ?>
 		</h1>
 
 	<?php elseif (is_author()) : ?>
 		<?php $author_id = $post->post_author; ?>
 		<h1 class="archive-title archive-author-title intro-title">
-			<span><?php _e('Posts By:', 'bonestheme'); ?></span> <?php the_author_meta('display_name', $author_id); ?>
+			<span><?php _e('Posts By:', 'wdg-theme'); ?></span> <?php the_author_meta('display_name', $author_id); ?>
 		</h1>
 
 	<?php elseif (is_day()) : ?>
 		<h1 class="archive-title archive-day-title intro-title">
-			<span><?php _e('Daily Archives:', 'bonestheme'); ?></span>
+			<span><?php _e('Daily Archives:', 'wdg-theme'); ?></span>
 			<?php the_time('F'); ?>&nbsp;<?php the_time('j'); ?>,&nbsp;<?php the_time('Y'); ?>
 		</h1>
 
 	<?php elseif (is_month()) : ?>
 		<h1 class="archive-title archive-month-title intro-title">
-			<span><?php _e('Monthly Archives:', 'bonestheme'); ?></span> <?php the_time('F Y'); ?>
+			<span><?php _e('Monthly Archives:', 'wdg-theme'); ?></span> <?php the_time('F Y'); ?>
 		</h1>
 
 	<?php elseif (is_year()) : ?>
 		<h1 class="archive-title archive-year-title intro-title">
-			<span><?php _e('Yearly Archives:', 'bonestheme'); ?></span> <?php the_time('Y'); ?>
+			<span><?php _e('Yearly Archives:', 'wdg-theme'); ?></span> <?php the_time('Y'); ?>
 		</h1>
 	
 	<?php endif; ?>
@@ -47,10 +47,10 @@
 					<?php
 						$categories = (is_single()) ? ' <span class="amp">&</span> filed under %4$s' : '';
 						printf(
-							__( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>' . $categories . '.', 'bonestheme' ),
+							__( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>' . $categories . '.', 'wdg-theme' ),
 							get_the_time('Y-m-j'),
 							get_the_time(get_option('date_format')),
-							bones_get_the_author_posts_link(),
+							get_the_author_link(),
 							get_the_category_list(', ')
 						);
 					?>
@@ -62,7 +62,7 @@
 		</article>
 
 	<?php endwhile; ?>
-	<?php bones_page_navi(); ?>
+	<?php posts_nav_link(); ?>
 </div>
 
 <?php get_sidebar(); ?>
