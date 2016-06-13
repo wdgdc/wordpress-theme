b803a319dfab1ef73f0fd51665228efb51c3e4c3
Kurtis Shaner <kurtis@shanerdesign.net>
2016-06-13 12:16:29 -0400

    Adding more wp-head cleanup

--- includes/class-wdg.php
+++ includes/class-wdg.php
@@ -183,6 +183,35 @@ class WDG {
 
 		// WP version
 		remove_action( 'wp_head', 'wp_generator' );
+
+		// Turn off oEmbed auto discovery.
+		remove_filter( 'oembed_dataparse' , 'wp_filter_oembed_result', 10 ); // Don't filter oEmbed results.
+		remove_action( 'wp_head' , 'wp_oembed_add_discovery_links' ); // Remove oEmbed discovery links.
+		remove_action( 'wp_head' , 'wp_oembed_add_host_js' ); // Remove oEmbed-specific JavaScript from the front-end and back-end.
+
+		// Remove rest api tag
+		remove_action( 'wp_head' , 'rest_output_link_wp_head' );
+
+		// Remove shortlink
+		remove_action( 'wp_head' , 'wp_shortlink_wp_head' );
+
+		// remove recentcomments style
+		add_action( 'widgets_init', function() {
+			global $wp_widget_factory;
+			remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+		} );
+
+		// disable emojis
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		add_filter( 'tiny_mce_plugins', function( $plugins ) {
+			return ( is_array( $plugins ) ) ? array_diff( $plugins, array( 'wpemoji' ) ) : array();
+		});
 	}
 
 	public static function register_sidebars() {
