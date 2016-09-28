49be680f5084de8e306008a96620384d10f8f47b
Joan Piedra <joan@webdevelopmentgroup.com>
2016-09-28 16:47:16 -0400

    feat(templates): styleguide template

---
+++ templates/styleguide.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Template name: Styleguide */
+
+the_post();
+get_header();
+
+?>
+
+<h1><?php the_title(); ?></h1>
+<?php the_content(); ?>
+
+<hr>
+
+<?php echo Theme::get_template_part( 'partials/kitchen-sink' ); ?>
+
+<hr>
+
+<?php
+
+get_footer();
