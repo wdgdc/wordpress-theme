342aa77d9ac6c37cea6059401dfc24b4bb83a982
Joan Piedra <joan@webdevelopmentgroup.com>
2016-10-13 17:07:45 -0400

    style(autoload): Matching WordPress coding standards

--- includes/autoload.php
+++ includes/autoload.php
@@ -2,13 +2,13 @@
 
 class Theme_Autoloader {
 	public static function autoload( $class_name ) {
-		if (!class_exists($class_name)) {
-			$class_file = THEME_INCLUDES_PATH . '/class-' . str_replace('_', '-', strtolower($class_name)) . '.php';
-			if (file_exists($class_file)) {
-				require_once($class_file);
+		if ( ! class_exists( $class_name ) ) {
+			$class_file = THEME_INCLUDES_PATH . '/class-' . str_replace( '_', '-', strtolower( $class_name ) ) . '.php';
+			if ( file_exists($class_file ) ) {
+				require_once $class_file;
 			}
 		}
 	}
 }
 
-spl_autoload_register('Theme_Autoloader::autoload');
+spl_autoload_register( 'Theme_Autoloader::autoload' );
