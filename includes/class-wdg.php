7de44f644b818b8e0a3c5bfe80867f122f0d7220
Kurtis Shaner <kshaner@webdevelopmentgroup.com>
2016-10-17 10:50:44 -0400

    remove dns-prefetch for emojis

--- includes/class-wdg.php
+++ includes/class-wdg.php
@@ -204,6 +204,15 @@ class WDG {
 		add_filter( 'tiny_mce_plugins', function( $plugins ) {
 			return ( is_array( $plugins ) ) ? array_diff( $plugins, array( 'wpemoji' ) ) : array();
 		});
+		add_filter( 'wp_resource_hints', function( $urls, $relation_type ) {
+			if ( 'dns-prefetch' == $relation_type ) {
+				/** This filter is documented in wp-includes/formatting.php */
+				$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+				$urls = array_diff( $urls, array( $emoji_svg_url ) );
+			}
+			return $urls;
+		}, 10, 2);
 	}
 
 	public static function register_sidebars() {
